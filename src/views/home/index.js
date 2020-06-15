fda06b1527aacb3a15ba6c5a2fd4ff2cc4c5cfbc
yichengliang <effiyichengliang@gmail.com>
2020-06-15 10:18:08 +0800

    feat: 添加滚动条样式

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import Styles from './styles.less'
+import mockNavData from '@/mock/mockNavData'
+import ScrollBar from '@/components/scrollbar'
 
 export default class Home extends React.Component {
 
@@ -14,7 +16,44 @@ export default class Home extends React.Component {
   render () {
     return (
       <div className="view-home">
-        hello world
+        <nav className="view-header">
+        <ScrollBar
+          className="nav-container"
+          onScrollY={() => logEvent('onScrollY')}
+          onScrollX={() => logEvent('onScrollX')}
+          onScrollUp={() => logEvent('onScrollUp')}
+          onScrollDown={() => logEvent('onScrollDown')}
+          onScrollLeft={() => logEvent('onScrollLeft')}
+          onScrollRight={() => logEvent('onScrollRight')}
+          onYReachStart={() => logEvent('onYReachStart')}
+          onXReachStart={() => logEvent('onXReachStart')}
+        >
+            {
+              mockNavData.map(item => (
+                <div key={item.key}>{item.value}</div>
+              ))
+            }
+        </ScrollBar>
+        </nav>
+        <div className="view-body">
+          <ScrollBar
+            className="body-container"
+            onScrollY={() => logEvent('onScrollY')}
+            onScrollX={() => logEvent('onScrollX')}
+            onScrollUp={() => logEvent('onScrollUp')}
+            onScrollDown={() => logEvent('onScrollDown')}
+            onScrollLeft={() => logEvent('onScrollLeft')}
+            onScrollRight={() => logEvent('onScrollRight')}
+            onYReachStart={() => logEvent('onYReachStart')}
+            onXReachStart={() => logEvent('onXReachStart')}
+          >
+              {
+                mockNavData.map(item => (
+                  <div key={item.key}>{item.value}</div>
+                ))
+              }
+          </ScrollBar>
+        </div>
       </div>
     )
   }
